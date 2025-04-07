1ed446c4cd164adcd387ad379b4aad2b818fcb1e
PeytonPlayz595 <106421860+PeytonPlayz595@users.noreply.github.com>
2025-04-06 20:38:33 -0700

    Add resources

---
+++ MakeOfflineDownload.bat
@@ -0,0 +1,4 @@
+@echo off
+title MakeOfflineDownload
+java -cp "desktopRuntime/MakeOfflineDownload.jar;desktopRuntime/CompileEPK.jar" net.lax1dude.eaglercraft.v1_8.buildtools.workspace.MakeOfflineDownload "javascript/OfflineDownloadTemplate.txt" "javascript/classes.js" "javascript/assets.epk" "javascript/Eaglercraft_Infdev_en_US.html"
+pause
