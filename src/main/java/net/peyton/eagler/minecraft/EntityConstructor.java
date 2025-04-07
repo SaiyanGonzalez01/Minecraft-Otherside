3bb5c78aaaa9b2926b24a601fdc4a7110e5771a6
PeytonPlayz595 <106421860+PeytonPlayz595@users.noreply.github.com>
2025-04-06 19:53:47 -0700

    Infdev source

---
+++ src/main/java/net/peyton/eagler/minecraft/EntityConstructor.java
@@ -0,0 +1,10 @@
+package net.peyton.eagler.minecraft;
+
+import net.minecraft.src.Entity;
+import net.minecraft.src.World;
+
+public interface EntityConstructor<T extends Entity> {
+	
+	T createEntity(World world);
+
+}
