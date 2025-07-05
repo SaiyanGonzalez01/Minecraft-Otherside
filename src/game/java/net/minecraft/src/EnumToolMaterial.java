3ae841f78d461f43b3fe0655abfc919fadb58658
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-05 15:22:28 -0400

    Update EnumToolMaterial.java

--- src/game/java/net/minecraft/src/EnumToolMaterial.java
+++ src/game/java/net/minecraft/src/EnumToolMaterial.java
@@ -3,16 +3,10 @@ package net.minecraft.src;
 public enum EnumToolMaterial {
 	WOOD(0, 60, 2.0F, 0),
 	STONE(1, 132, 4.0F, 1),
-	IRON(2, 250, 6.0F, 2),
-	EMERALD(3, 1562, 8.0F, 3),
-	GOLD(1, 500, 12.0F, 1),
-	UPWOOD(0, 80, 2.5F, 0),
-	UPSTONE(1, 162, 4.5F, 1),
-	UPIRON(2, 300, 6.5F, 2),
-	UPEMERALD(3, 1720, 8.5F, 3),
-	UPGOLD(1, 550, 12.5F, 1),
-	OBSIDIAN(4, 2562, 13.0F, 4),
-	UPOBSIDIAN(5, 2864, 14.0F, 5);
+	COPPER(2, 250, 6.0F, 2),
+	IRON(3, 320, 8.0F, 2),
+	EMERALD(5, 1562, 8.0F, 3),
+	GOLD(4, 800, 12.0F, 1),
 	
 	private final int harvestLevel;
 	private final int maxUses;
