f39f51498e976d3bdf1ff1a607e526d66c58a543
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-07-05 22:51:37 -0400

    Update Block.java

--- src/game/java/net/minecraft/src/Block.java
+++ src/game/java/net/minecraft/src/Block.java
@@ -87,7 +87,7 @@ public class Block {
 	public static final Block minecartTrack = (new BlockMinecartTrack(66, 128)).setHardness(1.0F).setStepSound(soundMetalFootstep);
 	public static final Block stairCompactStone = new BlockStairs(67, cobblestone);
 	public static final Block oreCopper = (new BlockOre(68, 51)).setHardness(3.0F).setResistance(5.0F).setStepSound(soundStoneFootstep);
-	public static final Block blockCopper = (new BlockOreBlock(69, 69)).setHardness(5.0F).setResistance(10.0F).setStepSound(soundMetalFootstep);
+	public static final Block blockCopper = (new BlockOreBlock(69, 85)).setHardness(5.0F).setResistance(10.0F).setStepSound(soundMetalFootstep);
 	public static final Block blastOvenIdle = (new BlockBlastFurnace(70, false)).setHardness(3.5F).setStepSound(soundStoneFootstep);
 	public static final Block blastOvenActive = (new BlockBlastFurnace(71, true)).setHardness(3.5F).setStepSound(soundStoneFootstep).setLightValue(14.0F / 16.0F);
 	public int blockIndexInTexture;
