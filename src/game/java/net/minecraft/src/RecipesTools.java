8bcd306e00585dd3fc7572f803fab5940d501bd7
Saiyan Gonzalez <153963453+SaiyanGonzalez01@users.noreply.github.com>
2025-09-20 22:02:29 -0400

    Add files via upload

--- src/game/java/net/minecraft/src/RecipesTools.java
+++ src/game/java/net/minecraft/src/RecipesTools.java
@@ -2,7 +2,7 @@ package net.minecraft.src;
 
 public class RecipesTools {
 	private String[][] recipePatterns = new String[][]{{"XXX", " # ", " # "}, {"X", "#", "#"}, {"XX", "X#", " #"}, {"XX", " #", " #"}};
-	private Object[][] recipeItems = new Object[][]{{Block.planks, Block.cobblestone, Item.ingotIron, Item.diamond, Item.ingotGold, Item.ingotCopper}, {Item.pickaxeWood, Item.pickaxeStone, Item.pickaxeSteel, Item.pickaxeDiamond, Item.pickaxeGold, Item.pickaxeCopper}, {Item.shovelWood, Item.shovelStone, Item.shovel, Item.shovelDiamond, Item.shovelGold, Item.shovelCopper}, {Item.axeWood, Item.axeStone, Item.axeSteel, Item.axeDiamond, Item.axeGold, Item.axeCopper}, {Item.hoeWood, Item.hoeStone, Item.hoeSteel, Item.hoeDiamond, Item.hoeGold, Item.hoeCopper}};
+	private Object[][] recipeItems = new Object[][]{{Block.planks, Item.ingotCopper, Item.ingotIron, Item.diamond, Item.ingotGold}, {Item.pickaxeWood, Item.pickaxeCopper, Item.pickaxeSteel, Item.pickaxeDiamond, Item.pickaxeGold}, {Item.shovelWood, Item.shovelCopper, Item.shovel, Item.shovelDiamond, Item.shovelGold}, {Item.axeWood, Item.axeCopper, Item.axeSteel, Item.axeDiamond, Item.axeGold}, {Item.hoeWood, Item.hoeCopper, Item.hoeSteel, Item.hoeDiamond, Item.hoeGold}};
 
 	public void addRecipes(CraftingManager var1) {
 		for(int var2 = 0; var2 < this.recipeItems[0].length; ++var2) {
